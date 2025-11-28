dfce086aca8cfd06bab5a83e1bf6bf38d1731fad
Philip Trozelli <94917108+PhilipTro@users.noreply.github.com>
2025-11-28 23:27:22 +0100

    some initial data exploration

--- src/data/preprocessing.py
+++ src/data/preprocessing.py
@@ -1,5 +1,26 @@
-import pandas
+import pandas as pd
 import numpy
+from sklearn import pipeline
 
-def load_data(csv_path):
-    return pd.read_csv(csv_path)
+FILEPATH = "../data/raw/insurance_claims.csv"
+
+def load_data(FILEPATH):
+    return pd.read_csv(FILEPATH)
+
+def clean_data(data:pd.DataFrame) -> pd.DataFrame:
+    cleaned_data = data.copy()
+    # Handling of duplicate data
+    if cleaned_data.duplicated().any():
+        cleaned_data = cleaned_data.drop_duplicates()
+    # Handling of outliers
+
+    # Handling of erroneus data
+
+    # handling missing data
+
+    
+    return cleaned_data
+    
+
+def preprocess(data):
+    pass
